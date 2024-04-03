a45c4697f2790f8a159172aca0f1038364f0a581
Cherepashhka <valkunariks@gmail.com>
2024-04-03 13:45:16 +0300

    Update

---
+++ Lab4_2/HomeElectronics.cs
@@ -0,0 +1,180 @@
+﻿using Lab4_2.ElectricDevices.FridgeDescendants;
+using Lab4_2.ElectricDevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Xml.Linq;
+using System.Reflection.Metadata;
+
+namespace Lab4_2
+{
+    public class HomeElectronics
+    {
+        private List<BaseElectricDevice> listofdevices = new List<BaseElectricDevice>();
+        
+        public void AddDevice(BaseElectricDevice device)
+        {
+            listofdevices.Add(device);  
+        }
+
+        public void Connect()
+        {
+                int index = 1;
+                Console.WriteLine("Choose a device: ");
+                foreach (var device in listofdevices)
+                {
+                    Console.WriteLine($"{index}. {device.Name}");
+                    index++;
+                }
+                index = int.Parse(Console.ReadLine());
+                listofdevices[index - 1].Connect();
+        }
+        public void Disconnect()
+        {
+                int index = 1;
+                Console.WriteLine("Choose a device: ");
+                foreach (var device in listofdevices)
+                {
+                    Console.WriteLine($"{index}. {device.Name}");
+                    index++;
+                }
+                index = int.Parse(Console.ReadLine());
+                listofdevices[index - 1].Disconnect();
+        }
+        public void ElectricitySum()
+        {
+            int sum = 0;
+            foreach (var device in listofdevices)
+            {
+                if (device.IsConnected)
+                {
+                    sum += device.ElectricityUsedInWatts;
+                }
+            }
+            Console.WriteLine("Electricity usage: " + sum);
+        }
+        public void Sort()
+        {
+            //for (int i = 0; i < listofdevices.Count; i++)
+            //{
+            //    for (int j = 0; j < listofdevices.Count - 1; j++)
+            //    {
+            //        if (listofdevices[j].ElectricityUsedInWatts > listofdevices[j + 1].ElectricityUsedInWatts)
+            //        {
+
+            //            BaseElectricDevice test = listofdevices[j + 1];
+            //            listofdevices[j + 1] = listofdevices[j];
+            //            listofdevices[j] = test;
+            //        }
+            //    }
+            //}
+            listofdevices = listofdevices.OrderBy(item => item.ElectricityUsedInWatts).ToList();
+            foreach (var device in listofdevices)
+            {
+                Console.WriteLine(device.ToString());
+            }
+        }
+        public void Search()
+        {
+            Console.WriteLine("Which parameter to set?\n1. Electricity usage.\n2. Years of warranty.\n3. Color\n4. Name");
+            int search;
+            search = int.Parse(Console.ReadLine());
+            if (search == 1)
+            {
+                search = 0;
+                int start, end;
+                do
+                {
+                    Console.WriteLine("Start: ");
+                    start = int.Parse(Console.ReadLine());
+                    Console.WriteLine("End: ");
+                    end = int.Parse(Console.ReadLine());
+                }
+                while (start < 0 || end <= start);
+                foreach (var device in listofdevices)
+                {
+                    if (device.ElectricityUsedInWatts > start && device.ElectricityUsedInWatts < end)
+                    {
+                        search++;
+                        Console.WriteLine(device.ToString());
+                    }
+                }
+            }
+            else if (search == 2)
+            {
+                search = 0;
+                int start, end;
+                do
+                {
+                    Console.WriteLine("Start: ");
+                    start = int.Parse(Console.ReadLine());
+                    Console.WriteLine("End: ");
+                    end = int.Parse(Console.ReadLine());
+                }
+                while (start < 0 || end <= start);
+                foreach (var device in listofdevices)
+                {
+                    if (device.YearsOfWarranty > start && device.YearsOfWarranty < end)
+                    {
+                        search++;
+                        Console.WriteLine(device.ToString());
+                    }
+                }
+            }
+            else if (search == 3)
+            {
+                search = 0;
+                string parameter;
+                Console.WriteLine("Enter color of device: ");
+                parameter = Console.ReadLine();
+                foreach (var device in listofdevices)
+                {
+                    if (device.Color == parameter)
+                    {
+                        search++;
+                        Console.WriteLine(device.ToString());
+                    }
+                }
+            }
+            else if (search == 4)
+            {
+                search = 0;
+                string parameter;
+                Console.WriteLine("Enter name of device: ");
+                parameter = Console.ReadLine();
+                foreach (var device in listofdevices)
+                {
+                    if (device.Name == parameter)
+                    {
+                        search++;
+                        Console.WriteLine(device.ToString());
+                    }
+                }
+            }
+            if (search == 0)
+                Console.WriteLine("Nothing has been found by these parameters!");
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is HomeElectronics homeelectronics)
+                return listofdevices == homeelectronics.listofdevices;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string? ToString()
+        {
+            string result = "";
+            foreach (var device in listofdevices)
+            {
+                result += device.ToString() + "\n";
+            }
+            return result;
+        }
+    }
+}
--- Lab4_2/Program.cs
+++ Lab4_2/Program.cs
@@ -5,141 +5,31 @@ using Lab4_2.ElectricDevices.FridgeDescendants;
 Fridge fridge = new Fridge();
 Freezer freezer = new Freezer();
 Stove stove = new Stove();
-MicrowaveOven microwaveoven =  new MicrowaveOven();
-Kettle kettle = new Kettle();
-List<BaseElectricDevice> listofdevices = new List<BaseElectricDevice>
-{
-    fridge,
-    freezer,
-    stove,
-    microwaveoven,
-    kettle
-};
+MicrowaveOven microwaveoven = new MicrowaveOven();
+Kettle kettle = new Kettle(400, 4, "DarkGray", "Kettle", 5);
+HomeElectronics homeelectronics = new HomeElectronics();
+homeelectronics.AddDevice(fridge);
+homeelectronics.AddDevice(freezer);
+homeelectronics.AddDevice(stove);
+homeelectronics.AddDevice(microwaveoven);
+homeelectronics.AddDevice(kettle);
 while (true)
 {
     Console.Clear();
+    Console.WriteLine(homeelectronics.ToString());
     Console.WriteLine("Pick a task:\n1. Turn on device.\n2. Turn off device.\n3. Sum of electricity usage.\n4. Sort by electricity.\n5. Find device.\n6. Exit.");
     int choice;
     choice = int.Parse(Console.ReadLine());
     if (choice == 1)
-    {
-        Console.WriteLine("Choose a device: ");
-        foreach (var device in listofdevices)
-        {
-            Console.WriteLine($"{choice}. {device.Name}");
-            choice++;
-        }
-        choice = int.Parse(Console.ReadLine());
-        listofdevices[choice - 1].Connect();
-    }
+        homeelectronics.Connect();
     else if (choice == 2)
-    {
-        choice = 1;
-        Console.WriteLine("Choose a device: ");
-        foreach (var device in listofdevices)
-        {
-            Console.WriteLine($"{choice}. {device.Name}");
-            choice++;
-        }
-        choice = int.Parse(Console.ReadLine());
-        listofdevices[choice - 1].Disconnect();
-    }
+        homeelectronics.Disconnect();
     else if (choice == 3)
-    {
-        int sum = 0;
-        foreach (var device in listofdevices)
-        {
-            if (device.IsConnected)
-            {
-                sum += device.ElectricityUsedInWatts;
-            }
-        }
-        Console.WriteLine("Electricity usage: " + sum);
-    }
+        homeelectronics.ElectricitySum();
     else if (choice == 4)
-    {
-        Sort(listofdevices);
-    }
+        homeelectronics.Sort();
     else if (choice == 5)
-    {
-        Console.WriteLine("Which parameter to set?\n1. Electricity usage.\n2. Years of warranty.\n3. Color\n4. Name");
-        int search;
-        search = int.Parse(Console.ReadLine());
-        if (search == 1)
-        {
-            search = 0;
-            int start, end;
-            do
-            {
-                Console.WriteLine("Start: ");
-                start = int.Parse(Console.ReadLine());
-                Console.WriteLine("End: ");
-                end = int.Parse(Console.ReadLine());
-            }
-            while (start < 0 || end <= start);
-            foreach (var device in listofdevices)
-            {
-                if (device.ElectricityUsedInWatts > start && device.ElectricityUsedInWatts < end)
-                {
-                    search++;
-                    Console.WriteLine(device.ToString());
-                }
-            }
-        }
-        else if (search == 2)
-        {
-            search = 0;
-            int start, end;
-            do
-            {
-                Console.WriteLine("Start: ");
-                start = int.Parse(Console.ReadLine());
-                Console.WriteLine("End: ");
-                end = int.Parse(Console.ReadLine());
-            }
-            while (start < 0 || end <= start);
-            foreach (var device in listofdevices)
-            {
-                if (device.YearsOfWarranty > start && device.YearsOfWarranty < end)
-                {
-                    search++;
-                    Console.WriteLine(device.ToString());
-                }
-            }
-        }
-        else if (search == 3)
-        {
-            search = 0;
-            string parameter;
-            Console.WriteLine("Enter color of device: ");
-            parameter = Console.ReadLine();
-            foreach (var device in listofdevices)
-            {
-                if (device.Color == parameter)
-                {
-                    search++;
-                    Console.WriteLine(device.ToString());
-                }
-            }
-        }
-        else if (search == 4) 
-        {
-            search = 0;
-            string parameter;
-            Console.WriteLine("Enter name of device: ");
-            parameter = Console.ReadLine();
-            foreach (var device in listofdevices)
-            {
-                if (device.Name == parameter)
-                {
-                    search++;
-                    Console.WriteLine(device.ToString());
-                }
-            }
-        }
-        if (search == 0)
-            Console.WriteLine("Nothing has been found by these parameters!");
-    }
+        homeelectronics.Search();
     else if (choice == 6)
     {
         Console.Clear();
@@ -151,26 +41,4 @@ while (true)
     }
     Console.WriteLine("Press any key to continue...");
     Console.ReadKey();
-}
-
-void Sort(List<BaseElectricDevice> list)
-{
-    //for (int i = 0; i < list.Count; i++)
-    //{
-    //    for (int j = 0; j < list.Count - 1; j++)
-    //    {
-    //        if (list[j].ElectricityUsedInWatts > list[j + 1].ElectricityUsedInWatts)
-    //        {
-
-    //            BaseElectricDevice test = list[j + 1];
-    //            list[j + 1] = list[j];
-    //            list[j] = test;
-    //        }
-    //    }
-    //}
-    list = list.OrderBy(item => item.ElectricityUsedInWatts).ToList();
-    foreach (var device in list)
-    {
-        Console.WriteLine(device.ToString());
-    }
 }
